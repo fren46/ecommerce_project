2afd193ffff37242423d33e6d07b981937b1b36a
Luigi Ferrettino <s291468@studenti.polito.it>
2021-03-04 12:44:42 +0100

    initial configuration set

---
+++ warehouseservice/src/main/kotlin/com/ecomm/warehouseservice/WarehouseserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.ecomm.warehouseservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WarehouseserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<WarehouseserviceApplication>(*args)
+}
---
+++ warehouseservice/src/test/kotlin/com/ecomm/warehouseservice/WarehouseserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ecomm.warehouseservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WarehouseserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
