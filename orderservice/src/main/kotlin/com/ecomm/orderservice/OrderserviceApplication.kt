95521a47571385e21528d68db1bf296f46a981a3
Luigi Ferrettino <s254300@studenti.polito.it>
2021-03-23 21:58:36 +0100

    added kafka-compose and updated gitignore

--- orderservice/src/main/kotlin/com/ecomm/orderservice/OrderserviceApplication.kt
+++ orderservice/src/main/kotlin/com/ecomm/orderservice/OrderserviceApplication.kt
@@ -1,11 +1,46 @@
 package com.ecomm.orderservice
 
+import com.ecomm.orderservice.dto.OrderDTO
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.support.beans
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.serializer.JsonDeserializer
+
 
 @SpringBootApplication
 class OrderserviceApplication
 
 fun main(args: Array<String>) {
-    runApplication<OrderserviceApplication>(*args)
+    runApplication<OrderserviceApplication>(*args){
+        addInitializers(
+            beans {
+                bean<KafkaTemplate<String, Any>> {
+                    val producerFactory = DefaultKafkaProducerFactory<String, Any>(mapOf(
+                        ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+                        ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to
+                                "org.apache.kafka.common.serialization.StringSerializer",
+                        ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to
+                                "org.springframework.kafka.support.serializer.JsonSerializer"))
+                    KafkaTemplate<String, Any>(producerFactory);
+                }
+                bean("OrderJsonListener"){
+                    val config = mapOf(
+                        ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092")
+                    val consumerFactory = DefaultKafkaConsumerFactory(
+                        config, StringDeserializer(), JsonDeserializer(OrderDTO::class.java)
+                    )
+                    val factory = ConcurrentKafkaListenerContainerFactory<String,
+                            OrderDTO>()
+                    factory.consumerFactory = consumerFactory;
+                    factory
+                }
+            }
+        )}
 }
