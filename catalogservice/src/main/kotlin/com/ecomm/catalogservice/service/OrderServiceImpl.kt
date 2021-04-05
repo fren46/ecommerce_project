952e39c58b2abb16e89e01306fe15011dafa4e6b
FRANCESCO VALENTE S291486 <s291486@studenti.polito.it>
2021-04-06 00:44:27 +0200

    added kafka listeners in OrderServiceImpl that listen on order status message and notify user with mail

---
+++ catalogservice/src/main/kotlin/com/ecomm/catalogservice/service/OrderServiceImpl.kt
@@ -0,0 +1,57 @@
+package com.ecomm.catalogservice.service
+
+import com.ecomm.catalogservice.repo.UserRepository
+import com.ecomm.commons.KafkaKeys
+import com.ecomm.commons.OrderDTO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.messaging.handler.annotation.Header
+import org.springframework.messaging.handler.annotation.Payload
+import org.springframework.stereotype.Service
+
+@Service
+class OrderServiceImpl(private val emailSender: JavaMailSender, private val userRepository: UserRepository): OrderService {
+
+    @Autowired
+    lateinit var kafkaTemplate: KafkaTemplate<String, OrderDTO>
+
+    fun sendEmail(
+        subject: String,
+        text: String,
+        targetEmail: String
+    ) {
+        val message = SimpleMailMessage()
+        message.setSubject(subject)
+        message.setText(text)
+        message.setTo(targetEmail)
+        emailSender.send(message)
+    }
+
+    @KafkaListener(topics = ["status"], groupId = "catalog")
+    override fun notifyClient(
+        @Payload dto: OrderDTO,
+        @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) key: String
+    ) {
+        if(key == KafkaKeys.KEY_ORDER_PAID.toString()){
+            var customer = userRepository.findById(dto.buyer!!)
+            sendEmail(
+                "[ECOMM][UPDATE] Your order is PAID",
+                "Dear Customer ${customer.name} ${customer.surname} your order with id ${dto.id} is PAID",
+                customer.email
+            )
+        }else if(key == KafkaKeys.KEY_ORDER_FAILED.toString()){
+            // TODO: 4/6/2021 make different failed key for each kind of error to notify the user with the correct error (not money or not availability)
+            var customer = userRepository.findById(dto.buyer!!)
+            sendEmail(
+                "[ECOMM][UPDATE] Your order is FAILED",
+                "Dear Customer ${customer.name} ${customer.surname} your order with id ${dto.id} is FAILED",
+                customer.email
+            )
+        }
+
+    }
+}
